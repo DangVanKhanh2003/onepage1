3b07632fa73a23a55774a08a7b13d0f451178148
khanhdepdai <dangvankhanh09022003@gmail.com>
2024-03-25 14:59:50 +0700

    past2

--- pageScript.js
+++ pageScript.js
@@ -20,6 +20,22 @@ $(document).ready(function(){
         $(this).find(".text-top-tag").css("color", "black");
         $(this).find(".number-tag").css("color", "black");
     });
-    
-    
+    $('#menuElemHome').click(function(){
+        $('html, body').animate({scrollTop: $('.home').offset().top }, 1000);
+    });
+    $('#menuElemServices').click(function(){
+        $('html, body').animate({scrollTop: $('#services').offset().top - 100}, 1000);
+    });
+    $('#menuElemAboutUs').click(function(){
+        $('html, body').animate({scrollTop: $('#about_us').offset().top - 100}, 1000);
+    });
+    $('#menuElemProject').click(function(){
+        $('html, body').animate({scrollTop: $('#project').offset().top - 100}, 1000);
+    });
+    $('#menuElemExpo2030').click(function(){
+        $('html, body').animate({scrollTop: $('#expo2030').offset().top - 100}, 1000);
+    });
+    $('#menuElemPartners').click(function(){
+        $('html, body').animate({scrollTop: $('#Ecological_construction').offset().top + 100}, 1000);
+    })
 })
